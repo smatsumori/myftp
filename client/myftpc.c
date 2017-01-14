19ed27f4b6a7a52f72140d00c9e4ab4be5984dd4
smatsumori <shoya.matsumori@gmail.com>
2017-01-14 18:11:02 +0900

    MOD: Implement proctable.

--- client/myftpc.c
+++ client/myftpc.c
@@ -1,12 +1,23 @@
 #include "./myftpc.h"
-#include "./tcpmodule.h"
 
 enum event_Flags {
-	EV_INIT, EV_TCP_CONNECTED, EV_TCP_DISCONNECT
+	EV_SENTINEL, EV_INIT_CMPL, EV_STDIN, EV_STDIN_INVALID, EV_RECV_PACKET,
+	EV_TIMEOUT, EV_EXIT,
 };
 
 enum status_Flags {
-	ST_INIT,
+	ST_SENTINEL, ST_INIT, ST_ESTABLISHED, ST_WAIT_PACKET, ST_WAIT_PACKET_RE,
+	ST_EXIT,
+};
+
+struct proctable ptab[] = {
+	{ST_INIT, EV_INIT_CMPL, tcpc_quick_establish, ST_ESTABLISHED},
+	{ST_ESTABLISHED, EV_STDIN, tcpc_send, ST_WAIT_PACKET},
+	{ST_ESTABLISHED, EV_STDIN_INVALID, dummy, ST_ESTABLISHED},
+	{ST_WAIT_PACKET, EV_TIMEOUT, tcpc_send, ST_WAIT_PACKET_RE},
+	{ST_WAIT_PACKET, EV_RECV_PACKET, dummy, ST_ESTABLISHED},		// TODO: remove dummy
+	{ST_WAIT_PACKET_RE, EV_TIMEOUT, tcpc_close, ST_EXIT},
+	{ST_SENTINEL, EV_SENTINEL, NULL, ST_SENTINEL}
 };
 
 struct command_table {	// TODO: ADD command here
--- client/myftpc.h
+++ client/myftpc.h
@@ -1,11 +1,25 @@
 #ifndef __HEADER_MYFTPC__
 #define __HEADER_MYFTPC__
 
+/*** PROTOTYPES ***/
+
+/*** INCLUDES ***/
+#include "../utils/utils.h"
+#include "../utils/packet.h"
+#include <netinet/in.h>
+
 struct myftpchead {
 	int mysockd;		/* socket descriptor for this client */
 	struct sockaddr_in servsockaddr;
 	struct myftp_packh packet_to_send;
 	struct myftp_packh packet_recieved;
+};
+
+struct proctable {
+	int status;
+	int event;	/* input */
+	void (*func)(struct dhcphead *hpr);
+	int nextstatus;
 };
 
 #endif	// __HEADER_MYFTPC__
