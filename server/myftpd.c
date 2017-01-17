c2b6648d61e1a3f0d9f5fb71ebaa4cde1698f82a
smatsumori <shoya.matsumori@gmail.com>
2017-01-18 00:57:22 +0900

    MOD: Add placeholder for global event handler.

--- server/myftpd.c
+++ server/myftpd.c
@@ -1,5 +1,31 @@
 #include "./mydhcpd.h"
 
+/*** FSM ***/
+enum globalevent_Flags {
+	GLOBAL_EV_RECVCONNREQ, GLOBAL_EV_CHILD_EXIT,
+};
+
+/*** PROTOTYPES ***/
+int global_event_dispatcher(struct myftpdhead *hpr);
+int global_client_selector(struct myftpdhead *hpr);
+
+/*** SIGNAL HANDLER ***/
+
+/*** FUNCTION ***/
+int 
+global_event_dispatcher(struct myftpdhead *hpr)
+{
+	// TODO: implement
+	return 0;
+}
+int 
+global_client_selector(struct myftpdhead *hpr)
+{
+	// TODO: implement
+	return 0;
+}
+
+/*** MAIN ***/
 int main(int argc, char const* argv[])
 {
 	
