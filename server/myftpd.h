aa4935045c072eee8c3412ddce1ba057fe793486
smatsumori <shoya.matsumori@gmail.com>
2017-01-18 00:55:56 +0900

    MOD: Add myftpdhead

--- server/myftpd.h
+++ server/myftpd.h
@@ -4,5 +4,14 @@
 #include "../utils/utils.h"
 #include "../utils/packet.h"
 
+struct myftpdhead {
+	int mysockd;		/* socket descriptor for this client */
+	int argc;
+	int cmd;
+	char argv[CMD_LENGTH][MAX_CMD];
+	struct sockaddr_in servsockaddr;
+	struct myftp_packh packet_to_send;
+	struct myftp_packh packet_recieved;
+};
 
 #endif	//endof: __HEADER_MYDHCPD__
