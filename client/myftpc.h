1e1fac76f3950c89cf395ab656215b61119b0ce8
Shoya Matsumori <shoya.matsumori@gmail.com>
2017-01-20 15:36:53 +0900

    MOD: Add help function in client command
    This code is crap but there is no need to fix this.

--- client/myftpc.h
+++ client/myftpc.h
@@ -6,14 +6,20 @@
 #include "../utils/packet.h"
 #include <netinet/in.h>
 #include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 #define CMD_LENGTH 20
+#define DIR_LEN 100
+
+#define ERR_EXECVP 10		// TODO: move below lines to utils.h
 
 /*** PROTOTYPES ***/
 struct myftpchead;
 struct typetable;
 struct proctable;
 int setcmd(struct myftpchead *hpr, char cmd[CMD_LENGTH]);
+int chdirw(char *dir);
 
 struct myftpchead {
 	int mysockd;		/* socket descriptor for this client */
@@ -21,6 +27,7 @@ struct myftpchead {
 	uint8_t type;
 	uint8_t code;
 	char argv[CMD_LENGTH][MAX_CMD];
+	char pwd[DIR_LEN];
 	char **data_to_send;		
 	struct sockaddr_in servsockaddr;
 	struct myftp_packh packet_to_send;
@@ -77,6 +84,7 @@ setcmd(struct myftpchead *hpr, char cmd[CMD_LENGTH])
 			if (strcmp(ptr->cmd, "__SENTINEL__") == 0) {
 				return 1;		// client command
 			} else if (strcmp(hpr->argv[0], ptr->cmd) == 0) {
+				// TODO: set code here
 				hpr->type = ptr->type;
 				return 0;		// ftp command
 			}
@@ -85,11 +93,62 @@ setcmd(struct myftpchead *hpr, char cmd[CMD_LENGTH])
 	return -1;
 }
 
+
 void
 exec_cmd(struct myftpchead *hpr)
 {	
-	// TODO: implement
-	fprintf(stderr, "Execute here\n");
+	char *homepath = getenv("HOME");
+	char cmd[CMD_LENGTH];
+	if (strcmp(hpr->argv[0], "exit") == 0) {
+		// TODO: implement
+		fprintf(stderr, "exit\n");
+		exit(0);
+	} else if (strcmp(hpr->argv[0], "lpwd") == 0) {
+		strcpy(hpr->pwd, getenv("PWD"));
+		printf("%s\n", hpr->pwd);
+	} else if (strcmp(hpr->argv[0], "ldir") == 0) {
+		if (hpr->argc == 1) {
+			system("ls");
+		} else {
+			// TODO: check if this works
+			sprintf(cmd, "ls %s", hpr->argv[1]);
+			system(cmd);
+		}
+	} else if (strcmp(hpr->argv[0], "lcd") == 0) {
+		if (hpr->argc == 1) {
+			chdirw((char *)homepath);	
+		} else if (1 < hpr->argc) {
+			if(chdirw(hpr->argv[1]) == -1) {
+				fprintf(stderr, "no such directory\n");
+				return;
+			}
+		} else {
+			return;
+		}
+	} else if (strcmp(hpr->argv[0], "help") == 0) {
+		/* this code is crappy */
+		fprintf(stderr, "[CMD] | [Argument] | [Description]\n");
+		fprintf(stderr, "quit | -- | quit myftpc\n");
+		fprintf(stderr, "pwd | -- | show current directory in server\n");
+		fprintf(stderr, "cd | path | move current directory in server\n");
+		fprintf(stderr, "dir | [path] | show files in current directory\n");
+		fprintf(stderr, "lpwd | -- | show current directory in client\n");
+		fprintf(stderr, "lcd | path | move current directory in client\n");
+		fprintf(stderr, "ldir | [path] | show files in current directory\n");
+		fprintf(stderr, "get | path1 [path2] | get flie from server\n");
+		fprintf(stderr, "put | path1 [path2] | send file to server\n");
+		fprintf(stderr, "help | -- | show help\n");
+	}
 	return;
 }
+
+int 
+chdirw(char *dir) {
+	char pwd[DIR_LEN];
+	int stat = chdir(dir);
+	char *newPWD = getcwd(pwd, sizeof(pwd));
+	setenv("PWD", newPWD, 1);
+	return stat;
+}
+
 #endif	// __HEADER_MYFTPC__
