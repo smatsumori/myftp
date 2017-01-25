31ea7670e6a5b58787388560ff2378785a93d2a4
smatsumori <shoya.matsumori@gmail.com>
2017-01-25 21:24:58 +0900

    MOD: Compelete implementing send and recv data.(server)

--- server/client.h
+++ server/client.h
@@ -2,12 +2,13 @@
 #define __FTP_CLIENT_HEADER__
 
 #include <stdio.h>
+#include <time.h>
 
 #define CMD_BUFLEN 256
 
 /*** PROTOTYPE ***/
 int client_handler(struct myftpdhead *hpr);
-int client_recv(int sockd, char *msg);
+int client_recv(int sockd, char *msg[CMD_BUFLEN]);
 int client_send_cwd(int sockd, char *msg);
 int client_send_pwd(int sockd, char *msg);
 int client_send_list(int sockd, char *msg);
@@ -20,10 +21,10 @@ int
 client_handler(struct myftpdhead *hpr)
 {
 	int clisockd = hpr->clisockd;
-	char msg[CMD_BUFLEN];
+	char *msg;
 	fprintf(stderr, "Client forked.\n");
 	while (1) {
-		switch (client_recv(clisockd, msg)) {
+		switch (client_recv(clisockd, &msg)) {
 			case FTP_CWD:
 				client_send_cwd(clisockd, msg);
 				break;
@@ -60,12 +61,11 @@ client_handler(struct myftpdhead *hpr)
 }
 
 int 
-client_recv(int sockd, char *msg)		// has side effects on msg
+client_recv(int sockd, char *msg[CMD_BUFLEN])		// has side effects on msg
 {
-	msg = NULL;
+	*msg = NULL;
 	int size;
-	char buf[FTP_MAX_RECVSIZE];
-	char obuf[FTP_DATASIZE];
+  static char obuf[FTP_DATASIZE];
 	struct myftp_packh myftphp;
 
 	while (1) {
@@ -80,9 +80,10 @@ client_recv(int sockd, char *msg)		// has side effects on msg
 			if ((size = recv(sockd, &obuf, FTP_DATASIZE, 0)) < 0)
 				report_error_and_exit(ERR_RECV, "client_recv");
 			fprintf(stderr, "arg: %s\n", obuf);
+			*msg = obuf;
 			break;
 		} else {
-			msg = NULL;
+			*msg = NULL;
 			return myftphp.type;
 		}
 	}
@@ -210,8 +211,10 @@ int
 client_send_list(int sockd, char *msg)
 {
 	FILE *fp = popen("ls", "r");
-	char buf[CMD_BUFLEN];	
-	while (fgets(buf, sizeof(buf), fp) != 0) {
+	char obuf[1000];
+	char buf[CMD_BUFLEN] = "";	
+	while (fgets(obuf, sizeof(obuf), fp) != NULL) {
+		strcat(buf, obuf);
 	}
 	int length = strlen(buf) * sizeof(char);
 	buf[length - 1] = '\0';
@@ -242,14 +245,15 @@ client_send_store_ack(int sockd, char *msg)
 	 * the current directory has a write 
 	 * permission 
 	 */
-	if (strlen(msg) != 0) {
+	fprintf(stderr, "hoge%s\n", msg);
+	if (msg != NULL) {
 		/* check directory exists */
 		DIR *dirp = opendir(msg);
 		if (dirp) {
 			fprintf(stderr, "Valid directory\n");
 			closedir(dirp);
 		} else if (ENOENT == errno) {
-			fprintf(stderr, "Directory does not exist. [%s]\n", msg);
+			fprintf(stderr, "Directory does not exist. [%s]\n", *msg);
 			client_send_code(sockd, 0x12, 0x00, NULL);
 			return -1;
 		} else if (EACCES == errno) {
@@ -270,15 +274,29 @@ client_recv_store(int sockd, char *dir)
 	fprintf(stderr, "---------- DATA ------------\n");
 	fprintf(stderr, "%s", recvfile);
 	fprintf(stderr, "--------------------------\n");
+	time_t t = time(NULL);
+	struct tm tm = *localtime(&t);
+	FILE *pFile;
+	char filename[100];
 	/* TODO: write to directory */
+	if (dir == NULL) {
+		sprintf(filename, "newfile-%d-%d-%d-%d-%d-%d", 
+				tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour,
+				tm.tm_min, tm.tm_sec);
+	} else {
+		sprintf(filename, "./%s/newfile-%d-%d-%d-%d-%d-%d", dir,
+				tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour,
+				tm.tm_min, tm.tm_sec);
+	}
+	pFile = fopen(filename, "w");
+	if (pFile != NULL) {
+		fputs (recvfile, pFile);
+		fclose(pFile);
+	}
+	fprintf(stderr, "file saved: %s\n", filename);
 	return 0;
 }
 
-int
-client_send_retr(int sockd, char *dir)
-{
-	return 0;
-}
 
 int
 client_send_retr_ack(int sockd, char *filename)
@@ -288,12 +306,12 @@ client_send_retr_ack(int sockd, char *filename)
 	 * the current directory has a write 
 	 * permission 
 	 */
-	FILE *pFile = fopen(filename);
+	FILE *pFile = fopen(filename, "r");
 	if (pFile) {
 		fprintf(stderr, "Valid file name: %s\n", filename);
 		fclose(pFile);
 	} else if (ENOENT == errno) {
-		fprintf(stderr, "File does not exist. [%s]\n", msg);
+		fprintf(stderr, "File does not exist. [%s]\n", filename);
 		client_send_code(sockd, 0x12, 0x00, NULL);
 		return -1;
 	} else if (EACCES == errno) {
@@ -301,6 +319,23 @@ client_send_retr_ack(int sockd, char *filename)
 		client_send_code(sockd, 0x12, 0x01, NULL);
 		return -2;
 	}
+	client_send_code(sockd, 0x10, 0x01, NULL);
+	return 0;
+}
+
+int
+client_send_retr(int sockd, char *filename)
+{
+	fprintf(stderr, "Sending RETR data\n");
+	char line[1000];
+	static char buf[6000];
+	FILE *pFile = fopen(filename, "r");
+
+	while (fgets(line, 6000, pFile) != NULL) {
+		strcat(buf, line);
+	}
+	client_send_data(sockd, buf);
+	fclose(pFile);
 	return 0;
 }
 #endif	// End of __FTP_CLIENT_HEADER__
